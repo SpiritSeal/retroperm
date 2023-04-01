264b7b1d4eea8018591fe7d3031c48360bd9bc66
spyre <yssaketh@gmail.com>
2023-04-01 16:08:58 -0700

    Cleaned (more) reference 4

--- reference/4_vex_rev_hook.py
+++ reference/4_vex_rev_hook.py
@@ -1,17 +1,20 @@
+"""
+This reference scripts shows a proof of concept for how to use the VEX IR to find arguments passed to a function call.
+"""
 import angr
 import pyvex
-
 from reference.utils_angrmgmt import string_at_addr
-from retroperm.analysis.utils import explode
-from utils import get_arg_locations, explore
 from data import important_func_args
-
 import logging
 
+
+# Disable annoying warning messages
 logging.getLogger('angr.analyses.reaching_definitions').setLevel(logging.FATAL)
 logging.getLogger('angr.project').setLevel(logging.FATAL)
 logging.getLogger('cle.loader').setLevel(logging.FATAL)
 
+
+# Load the binary and perform control flow analysis and calling convention analysis
 proj = angr.Project('../executables/open_example', auto_load_libs=False)
 cfg = proj.analyses.CFGFast.prep()()
 ccca = proj.analyses[angr.analyses.CompleteCallingConventionsAnalysis].prep()(recover_variables=True)
@@ -26,8 +29,7 @@ for func in cfg.kb.functions.values():
             continue
         # Vex block
         vex_block: pyvex.block.IRSB = block.vex
-        if(vex_block.jumpkind == 'Ijk_Call'):
-            # print(vex_block)
+        if vex_block.jumpkind == 'Ijk_Call':
             # print("Found call at", hex(block.addr))
             try:
                 # print("Call goes to", vex_block.next.constants[0].value)
@@ -48,7 +50,7 @@ for func in cfg.kb.functions.values():
                         print("Important function:", simproc.__class__, important_func_args[simproc.__class__])
                         important_arg_nums = important_func_args[simproc.__class__]
                         # Get the calling convention of the target function
-                        target_arg_locations = get_arg_locations(ccca.kb.functions[call_target])
+                        target_arg_locations = ccca.kb.functions[call_target].arguments
                         target_arg_locations = [arg.reg_name for arg in target_arg_locations]
                         important_args = [target_arg_locations[arg_num] for arg_num in important_arg_nums]
                         print("important_args:", important_args)
@@ -57,58 +59,19 @@ for func in cfg.kb.functions.values():
                         simproc_prototype = simproc.prototype
                         print("simproc_prototype:", simproc_prototype)
                         print("simproc_prototype.args:", simproc_prototype.args)
-                        # explore(simproc_prototype.args[1])
 
                         for stmt in vex_block.statements:
-                            # print("stmt:", stmt)
                             if isinstance(stmt, pyvex.stmt.Put):
                                 reg = proj.arch.register_names[stmt.offset]
-                                # print(reg)
                                 if reg in important_args:
                                     print(reg)
                                     # Find the argument number
                                     arg_num = important_args.index(reg)
                                     if simproc_prototype.args[arg_num].__class__ == angr.sim_type.SimTypePointer:
+                                        # Print the string at the pointer
                                         print(string_at_addr(cfg, stmt.data.con.value, proj))
                                     elif simproc_prototype.args[arg_num].__class__ == angr.sim_type.SimTypeInt:
+                                        # Print the integer
                                         print(hex(stmt.data.con.value))
                                     else:
                                         print("Unknown type:", simproc_prototype.args[arg_num].__class__)
-
-'''
-        for stmt in vex_block.statements:
-            if stmt.tag == 'Ist_IMark':
-                call_blocks.add(block.addr)
-                continue
-                call_target = stmt.addr
-                call_target_symbol = cfg.kb.functions.function(addr=call_target)
-
-                if call_target_symbol is not None:
-                    # Check if symbol is hooked
-                    if not proj.is_symbol_hooked(call_target_symbol.name):
-                        continue
-                    simproc = proj.symbol_hooked_by(call_target_symbol.name)
-                    if simproc:
-                        if simproc.__class__ in important_func_args:
-                            print("Important function:", simproc.__class__, important_func_args[simproc.__class__])
-                            important_arg_nums = important_func_args[simproc.__class__]
-                            # Get the calling convention of the target function
-                            target_arg_locations = get_arg_locations(ccca.kb.functions[call_target])
-                            target_arg_locations = [arg.reg_name for arg in target_arg_locations]
-                            important_args = [target_arg_locations[arg_num] for arg_num in important_arg_nums]
-                            print("important_args:", important_args)
-
-                            for stmt in vex_block.statements:
-                                print("here")
-                                # print("stmt:", stmt)
-                                if isinstance(stmt, pyvex.stmt.Put):
-                                    reg = proj.arch.register_names[stmt.offset]
-                                    # print(reg)
-                                    if reg in important_args:
-                                        print(string_at_addr(cfg, stmt.data.con.value, proj))
-
-                            # for reg in important_args:
-                            #     print("Found arg:", reg)
-                            #     print(string_at_addr(cfg, stmt.data.con.value, proj))
-# print(call_blocks)
-'''
