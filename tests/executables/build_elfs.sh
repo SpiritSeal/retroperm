f7395c785d7a1c5899834e6349127c59d75336df
spyre <yssaketh@gmail.com>
2023-04-13 16:26:03 -0700

    Shell script to Auto Build script for c-based elf binaries

--- tests/executables/build_elfs.sh
+++ tests/executables/build_elfs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-gcc src/socket_server_example.c -o socket_server_example
-gcc src/socket_client_example.c -o socket_client_example
+CURRENT_DIR=$(pwd)
+cd "$(dirname "$0")" || return
+
+for file in ./src/*.c; do
+  gcc "$file" -o "$(basename "${file%.c}")"
+done
+
+cd "$CURRENT_DIR" || return
